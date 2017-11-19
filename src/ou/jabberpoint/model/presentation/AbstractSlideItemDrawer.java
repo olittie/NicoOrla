33a9522c556f0280811cd32d3d526e49373d8833
FleCZ <FleCZ@192.168.1.72>
2017-11-19 22:37:14 +0100

    laatste versie

--- src/ou/jabberpoint/model/presentation/AbstractSlideItemDrawer.java
+++ src/ou/jabberpoint/model/presentation/AbstractSlideItemDrawer.java
@@ -1,15 +1,12 @@
 package ou.jabberpoint.model.presentation;
 
 import java.awt.Graphics;
-import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
-import java.util.Vector;
 
 public abstract class AbstractSlideItemDrawer {
 	public void drawText(Graphics g, int x, int y, float scale, Style style, String text) { }
     public void drawBitmap(Graphics g, int x, int y, float scale, Style style, BufferedImage image) { }
-    void drawLines(Graphics g, Style style, Vector<Point> connectingPoints) { }
 
     public Rectangle getBoundingBox(Graphics g, float scale, Style style, BufferedImage image) { return new Rectangle(); }
     public Rectangle getBoundingBox(Graphics g, float scale, Style style, String text) { return new Rectangle(); }
